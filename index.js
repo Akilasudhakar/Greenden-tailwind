b157a62d4350d7db564e27b25646a05850a72ead
Akilasudhakar <akilasudhakar11@gmail.com>
2025-03-06 19:55:30 +0530

    Added JS

---
+++ index.js
@@ -0,0 +1,15 @@
+// Selecting sidenav and menuicon
+
+
+var menuicon=document.getElementById("menuicon")
+var sidenav=document.getElementById("sidenav")
+var closenav=document.getElementById("close-nav")
+
+menuicon.addEventListener("click",function(){
+    sidenav.style.right=0
+
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
